701cea876f0b59b22abec88b1463c194c0b82ec4
BetsyTang <tangjingjingbetsy@gmail.com>
2021-02-27 23:33:43 +0800

    change log info

--- main.py
+++ main.py
@@ -33,10 +33,16 @@ def checkpoint(net, save_path, loss, iterations):
 
 iterations = 0
 
+
+header = '  Time Epoch Iteration Progress    (%Epoch)   Loss'
+log_template = ' '.join('{:>6.0f},{:>5.0f},{:>9.0f},{:>5.0f}/{:<5.0f} {:>7.1f}%,{:>7.4f}'.split(','))
+
 print("Start training...")
+start = time.time()
 for epoch in range(50):  # loop over the dataset multiple times
     print_loss = 0
     n = 0
+    
     for i, data in enumerate(triplet_data, 0):
         iterations += 1
         # get the inputs
@@ -47,12 +53,15 @@ for epoch in range(50):  # loop over the dataset multiple times
         # forward + backward + optimize
         output_1,output_2,output_3 = net.forward(inputs[0].to(device),inputs[1].to(device),inputs[2].to(device))
         loss = criterion.forward(output_1, output_2, output_3) 
-        print_loss += loss
+        print_loss += loss.item()
         n += 1
         loss.backward()
         optimizer.step()  
 
         if iterations % 100 == 0:
             checkpoint(net, "log/", print_loss/n, iterations)
+            print(log_template.format(time.time()-start,
+                    epoch, iterations, 1+i, len(triplet_data),
+                    100. * (1+i) / len(triplet_data), print_loss/n))
 
 torch.save(net, "final_model.pt")
