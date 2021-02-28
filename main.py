f5f3273bc54ba70239e26a89987961bf6a677746
BetsyTang <tangjingjingbetsy@gmail.com>
2021-02-28 18:39:00 +0800

    update network

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ for epoch in range(40):  # loop over the dataset multiple times
         # print(inputs.shape)
         # zero the parameter gradients
         optimizer.zero_grad()
-        # inputs = [torch.reshape(i, i.shape[1:]) for i in inputs]
+        inputs = [torch.reshape(i, i.shape[1:]) for i in inputs]
         # forward + backward + optimize
         output_1,output_2,output_3 = net.forward(inputs[0].to(device),inputs[1].to(device),inputs[2].to(device))
         loss = criterion.forward(output_1, output_2, output_3) 
