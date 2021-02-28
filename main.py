daa9d6286c02d6d52dcf25576340bfb67553292a
BetsyTang <tangjingjingbetsy@gmail.com>
2021-02-28 22:24:17 +0800

    change log para

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ for epoch in range(40):  # loop over the dataset multiple times
         loss.backward()
         optimizer.step()  
 
-        if iterations % 5000 == 0:
+        if iterations % 100 == 0:
             checkpoint(net, "log/", print_loss/n, iterations)
             print(log_template.format(time.time()-start,
                     epoch, iterations, 1+i, len(triplet_data),
